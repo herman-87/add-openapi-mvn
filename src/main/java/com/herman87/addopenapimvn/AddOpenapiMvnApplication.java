8a135903063544f77370046d03beaa7ede22f5c9
essoungou.wonga <essoungou.wonga@lao-sarl.cm>
2023-06-10 15:31:53 +0100

    trainning:openapi: Add open api using spring boot + maven

---
+++ src/main/java/com/herman87/addopenapimvn/AddOpenapiMvnApplication.java
@@ -0,0 +1,13 @@
+package com.herman87.addopenapimvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddOpenapiMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddOpenapiMvnApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/herman87/addopenapimvn/api/AccountResources.java
@@ -0,0 +1,7 @@
+package com.herman87.addopenapimvn.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountResources {
+}
